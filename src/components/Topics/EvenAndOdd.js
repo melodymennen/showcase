912a4b25c6947c9e2b12997e432f601862eac65c
Melody <Melody@Melodys-MacBook-Pro.local>
2017-12-01 15:14:11 -0700

    continuing step 3

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -9,15 +9,34 @@ class EvenAndOdd extends Component {
             userInput: ''
         }
     }
+
+    handleChange(value) {
+        this.setState({userInput: value})
+    }
+
+    assignEvenAndOdds(userInput){
+        var arr = userInput.split('')
+        var evens = []
+        var odds = []
+        for (let i=0;i<arr.length;i++){
+            if (arr[i]%2===0){
+                evens.push(parseInt(arr[i],10))
+            }
+            else if (arr[i]%2!==0){
+                odds.push(parseInt(arr[i],10))
+            }
+        }
+        this.setState({evenArray:evens, oddArray:odds})
+    }
     
     render() {
         return (
            <div className='puzzleBox evenAndOddPB'>
                 <h4>Evens and Odds</h4>
-                <input className='inputLine'/>
-                <button className='confirmationButton'></button>
-                <span className='resultsBox'></span>
-                <span className='resultsBox'></span>
+                <input className='inputLine' onChange={event => this.handleChange(event.target.value)}/>
+                <button className='confirmationButton' onClick={() => {this.assignEvenAndOdds(this.state.userInput)}}>Split</button>
+                <span className='resultsBox'>Evens: {JSON.stringify(this.state.evenArray)}</span>
+                <span className='resultsBox'>Odds: {JSON.stringify(this.state.oddArray)}</span>
            </div>
         )
     }
